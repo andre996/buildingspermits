3e521019a5bf3d846404156031e52f6a1d200378
andre996 <arrm.1996@gmail.com>
2023-07-12 16:57:15 -0600

    test: all commit

--- pages/map.py
+++ pages/map.py
@@ -1,12 +1,19 @@
 import dash
 from dash import html, dcc;
 import dash_design_kit as ddk
+import requests
+import pandas as pd
+import json
 
+
+# Import and convert data
+r = requests.get('https://data.calgary.ca/resource/8ced-xbvn.json')
+df = pd.read_json(json.dumps(r.json()))
 
 dash.register_page(__name__, image='assets\share.jpg')
 
 layout =  ddk.Card(
             children=[
-               ddk.CardHeader(title='Building inspection map')
+               ddk.CardHeader(title='Building Inspection Map')
             ]
 )
