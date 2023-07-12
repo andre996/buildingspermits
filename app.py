225d1250980ea2dac72ec6c6c8b99e0b3109b9be
andre996 <arrm.1996@gmail.com>
2023-07-12 16:30:34 -0600

    fix: requirements.txt

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from dash import Dash, html, dcc, callback, Output, Input, dash_table, page_registry, page_container
 import dash_design_kit as ddk
 import requests
@@ -187,53 +186,3 @@ app.layout = ddk.App(show_editor=True, theme=theme,children=[
 
 if __name__ == '__main__':
     app.run(debug=True)
-=======
-from dash import Dash, dcc, html, Input, Output
-import dash_design_kit as ddk
-import plotly.express as px
-
-app = Dash(__name__)
-server = app.server  # expose server variable for Procfile
-
-df = px.data.stocks()
-
-app.layout = ddk.App([
-    ddk.Header([
-        ddk.Logo(src=app.get_asset_url('logo.png')),
-        ddk.Title('Dash Enterprise Sample Application'),
-    ]),
-    ddk.Row(children=[
-        ddk.Card(children=[
-            ddk.CardHeader(children=[
-                dcc.Dropdown(
-                    id='title-dropdown',
-                    options=[{'label': i, 'value': i}
-                        for i in ['GOOG', 'AAPL', 'AMZN']],
-                    value='GOOG'
-                )
-            ]),
-            ddk.Graph(id='update-graph', style={'height':300}),
-        ]),
-    ]),
-
-    ddk.Row(children=[
-        ddk.Card(width=50, children=ddk.Graph(figure=px.line(df, x="date", y=["AMZN", "FB"], title='Stock Prices'))),
-
-        ddk.Card(width=50, children=ddk.Graph(figure=px.line(df, x="date", y=["AAPL", "MSFT"], title='Stock Prices')))
-    ])
-])
-
-
-@app.callback(Output('update-graph', 'figure'),
-              [Input('title-dropdown', 'value')])
-def update_graph(value):
-    if value == 'GOOG':
-        return px.line(df, x="date", y="GOOG", title='Google Stock Price')
-    elif value == 'AAPL':
-        return px.line(df, x="date", y="AAPL", title='Apple Stock Price')
-    elif value == 'AMZN':
-        return px.line(df, x="date", y="AMZN", title='Amazon Stock Price')
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
->>>>>>> c1bc40fc349df1b080e88853b9367e72596ee338
